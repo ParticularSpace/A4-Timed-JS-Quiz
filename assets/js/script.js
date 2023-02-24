d372a5017beada3f1123509aa31382f97c165e1e
Sam Jones <samejones2018@gmail.com>
2023-02-23 21:34:28 -0800

    created questions object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
 var answers = [{}];
-var questions = [{}];
+var questions = [{
+    question: "What is the capital of the United States?",
+    choices: ["New York", "Washington DC", "Los Angeles", "Chicago"],
+    correctAnswer: 1,
+}];
 var currentQuestion = 0;
 var score = 0;
 var currentTime = 0;
+
+//  when i click start quiz you should see a question if you get it right you should see the next question and a message saying you got it correct if you get it wrong you will lose 15 seconds from your timer and you will see the next question. then you will see highscores at the end with persistent data when completed you can enter a name to save your score.
+
+// set intervel for Timer to start when you click start quiz
+
+// const myQuestions = [
