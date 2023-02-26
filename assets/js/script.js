67f1dc256b5f3f005aae70eef64c46f88cb99316
Sam Jones <samejones2018@gmail.com>
2023-02-26 14:10:15 -0800

    created js actions to button clicks final score timer and active score work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,138 @@
-var answers = [{}];
-var questions = [{
-    question: "What is the capital of the United States?",
-    choices: ["New York", "Washington DC", "Los Angeles", "Chicago"],
-    correctAnswer: 1,
-}];
 var currentQuestion = 0;
 var score = 0;
-var currentTime = 0;
+var timer = 50;
+var startButton = document.getElementById("start-btn");
+var quizQuestion = document.getElementById("quiz-question");
+var quizAnswers = document.getElementById("quiz-answers");
+var quizTimer = document.getElementById("quiz-timer");
+var quizScore = document.getElementById("quiz-score");
+var quizEnd = document.getElementById("quiz-end");
 
-//  when i click start quiz you should see a question if you get it right you should see the next question and a message saying you got it correct if you get it wrong you will lose 15 seconds from your timer and you will see the next question. then you will see highscores at the end with persistent data when completed you can enter a name to save your score.
+var countdown;
 
-// set intervel for Timer to start when you click start quiz
 
-// const myQuestions = [
+
+// Start the quiz
+
+startButton.addEventListener("click", function() {
+    // Hide the start button
+    startButton.style.display = "none";
+    // Show the quiz
+    document.querySelector('.quiz').classList.remove('hide');
+    // Call the showQuestions() function to display the first question
+    showQuestions();
+    // Start the timer
+    countdown = setInterval(function() {
+        timer--;
+        quizTimer.textContent = timer;
+
+        if (timer <= 0) {
+            clearInterval(countdown);
+            endQuiz();
+        }
+    }, 1000);
+});
+
+const myQuestions = [
+    {
+    question: "Which of the following is written correctly?",
+    answers: {
+         a: "Helloworld",
+         b: "helloworld",
+         c: "HelloWorld",
+         d: "helloWorld"
+        },
+    correctAnswer: 'd',
+},
+{
+    question: "What is this quiz about?",
+    answers: {
+         a: "Top Spotify Artists",
+         b: "How to roast a chicken",
+         c: "About my favorite food sushi",
+         d: "How to be a pro at javascript"
+        },
+    correctAnswer: 'd',
+},
+{
+    question: "Where can you learn to code javaScript?",
+    answers: {
+         a: "UC Berkeley Coding Boot camp Extension",
+         b: "Watch netflix",
+         c: "Socializing with friends",
+         d: "Having a life"
+        },
+    correctAnswer: 'a',
+},
+{
+    question: "Whats the best music to listen too while coding javaScript?",
+    answers: {
+         a: "EDM!",
+         b: "Jazz",
+         c: "lof-fi",
+         d: "ANYTHING but country"
+        },
+    correctAnswer: 'd',
+},
+{
+    question: "Do you eat while you code javaScript?",
+    answers: {
+         a: "I have the dustiest keyboard in the world",
+         b: "I have the cleanest keyboard in the world",
+         c: "Its impossible to eat and code at the same time",
+         d: "We aren't perfect but we aren't slobs either"
+        },
+    correctAnswer: 'd',
+  }
+];
+
+function showQuestions() {
+    var myQuestion = myQuestions[currentQuestion];
+    quizQuestion.textContent = myQuestion.question;
+    quizAnswers.innerHTML = "";
+    for (var answer in myQuestion.answers) {
+        var button = document.createElement("button");
+        button.textContent = answer + ": " + myQuestion.answers[answer];
+        button.setAttribute("value", answer);
+        button.addEventListener("click", checkAnswer);
+        quizAnswers.appendChild(button);
+    }
+};
+
+ // i dont want an alert to pop up when i click on the correct answer
+function checkAnswer() {
+    if (this.value === myQuestions[currentQuestion].correctAnswer) {
+        score++;
+        quizScore.textContent = score;
+    } else {
+        timer -= 10;
+    }
+    currentQuestion++;
+    if (currentQuestion === myQuestions.length) {
+        endQuiz();
+    } else {
+        showQuestions();
+    }
+};
+
+function endQuiz() {
+        clearInterval(countdown);
+        quizQuestion.textContent = "All done!";
+        quizAnswers.innerHTML = "";
+        quizEnd.classList.remove("hide");
+        quizEnd.textContent = "Your final score is " + score + " out of " + myQuestions.length + "!";
+    }
+
+function saveScore() {
+    var initials = document.querySelector("#initials").value;
+    var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+    var newScore = {
+        score: score,
+        initials: initials
+    };
+    highScores.push(newScore);
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.href = "highscores.html";
+}
+    
+
